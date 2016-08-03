b22f98ba61038ff5f7e8c20f0189aad47e3f93c1
Pat Dayton <daytonpe14@gmail.com>
2016-08-03 17:42:43 -0500

    Added Cart and Other Part II Features

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,23 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'storefront#all_items'
 
-  get 'categorical' => 'storefront#items_by_category'
+  post 'add_to_cart' => 'cart#add_to_cart'
+  get 'view_order' => 'cart#view_order'
+  get 'checkout' => 'cart#checkout'
 
-  get 'branding' => 'storefront#items_by_brand'
+  resources :line_items
+  resources :orders
+  devise_for :users
 
   get 'all' => 'storefront#all_items'
+  get 'categorical' => 'storefront#items_by_category'
+  get 'branding' => 'storefront#items_by_brand'
+
+  resources :products
+  resources :categories
+
 
 end
+
+
---
+++ db/migrate/20160803211914_create_line_items.rb
@@ -0,0 +1,13 @@
+class CreateLineItems < ActiveRecord::Migration[5.0]
+  def change
+    create_table :line_items do |t|
+      t.string :resource
+      t.integer :product_id
+      t.integer :quantity
+      t.decimal :line_item_total
+      t.integer :order_id
+
+      t.timestamps
+    end
+  end
+end
